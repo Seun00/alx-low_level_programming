2d5b353fe390934e2d42d2aa2af846a932a85c33
sobande <sobandesolomon@gmail.com>
2022-09-08 06:11:27 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
+/**
+ * main
+ *
+ * Return: 0 after printing the function
+ */
 int main(void)
-{  
- puts("\"Programming is like building a multilingual puzzle");
- return (0);
+{puts("\"Programming is like building a multilingual puzzle");
+       return (0);
 }    
