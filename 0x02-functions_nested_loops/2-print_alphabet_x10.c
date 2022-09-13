4708aa9bde0ceb784c755a1be63f9c61471e7e1f
sobande <sobandesolomon@gmail.com>
2022-09-13 05:00:34 -0700

    Task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print alphabet_x10 - print alphabet 10 times
+ *
+ * Return: Always 0;
+ *
+ */
+void print_alphabet_x10(void)
+{
+	char i;
+	char h;
+
+	for (h = 0; h <= 9; h++;)
+	{
+		for (i = 'a'; i <= 'z'; 1++)
+		{
+			_putchar(i);
+		}
+		_putchar('\n');
+	}
+}
