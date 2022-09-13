c43ea4fc0ed4722d9735c519f2389fe5dc8f38db
sobande <sobandesolomon@gmail.com>
2022-09-13 10:44:30 -0700

    Task 12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -25,10 +25,17 @@ void print_times_table(int n)
 				{
 					_putchar(',');
 					_putchar(' ');
-					_putchar('0' + (prod / 100));
+					_putchar(' ');
+					_putchar(' ');
+					_putchar('0' + prod);
+				} else if (prod > 99)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar('0' + (prod / 10));
 					_putchar('0' + ((prod / 10) % 10));
 					_putchar('0' + (prod % 10));
-				} else
+				} else 
 				{
 					_putchar(',');
 					_putchar(' ');
