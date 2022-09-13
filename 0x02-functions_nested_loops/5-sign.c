8420a87b82b63cfbcc671b3babbd0bd700992380
sobande <sobandesolomon@gmail.com>
2022-09-13 05:58:15 -0700

    Task 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,10 +2,12 @@
 /**
  * print_sign - main function
  *
- * @n: character to compare 
+ * @n: character to compare
+ *
  * Return: 1 if positive, 0 if zero, -1 if negative
  *
  */
+
 int print_sign(int n)
 {
 	if (n > 0)
