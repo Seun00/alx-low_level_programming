efb8647186470cf06059b18989738ede071ad335
sobande <sobandesolomon@gmail.com>
2022-09-15 08:42:39 -0700

    Task 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,12 +8,14 @@ void more_numbers(void)
 	int i;
 	int h;
 
-	for (h = '0'; h <= '9'; h++)
+	for (h = 0; h <= 9; h++)
 	{
-		for (i = '0'; i <= '14'; i++)
+		for (i = 0; i <= 14; i++)
 		{
 			if (i > 9)
+			{
 				_putchar((i / 10) + '0');
+			}
 			_putchar((i % 10) + '0');
 		}
 		_putchar('\n');
