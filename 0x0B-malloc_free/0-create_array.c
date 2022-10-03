46e89bd8581f0629a210ee6f56dde0a6432faa10
sobande <sobandesolomon@gmail.com>
2022-10-03 08:07:59 -0700

    Task 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -16,7 +16,7 @@ char *create_array(unsigned int size, char c)
 		return (NULL);
 	t = malloc(sizeof(char) * size);
 	if (t == NULL)
-		if (t == NULL);
+		return (NULL);
 	for (i = 0; i < size; i++)
 		t[i] = c;
 	return (t);
