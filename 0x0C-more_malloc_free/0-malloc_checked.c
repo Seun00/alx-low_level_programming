8324153d0cd13f0293d915993552a66ac06cf151
sobande <sobandesolomon@gmail.com>
2022-10-06 03:47:36 -0700

    Task o

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * malloc_checked - checks malloc
+ * @b: integer to check?
+ * Return: a pointer or void
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *t;
+
+	
