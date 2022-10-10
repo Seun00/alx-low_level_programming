d5faad4089d6e899f92526516df16df84fbfb475
sobande <sobandesolomon@gmail.com>
2022-10-10 04:26:56 -0700

    Task 2

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - file returns file name
+ * Return: return 0 always
+ */
+int main(void)
+{
+	printf("file :%s\n", __File__);
+	return (0);
+}
